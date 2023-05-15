bc73d65576afb329565ef5937ed06e968d6ae867
heissy <heisycornejo@gmail.com>
2023-05-15 09:58:48 -0700

    Added enviroment
    added trees and chocolate.

---
+++ ChocolateRoadPeriod2/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 6.0f;
+    private Rigidbody playerRb;
+    private float zBound = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ContrainPlayerPosition();
+    }
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+    }
+    //prvent the player from leaving the top of the screen
+    void ContrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+}
