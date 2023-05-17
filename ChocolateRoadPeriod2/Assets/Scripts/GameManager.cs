55a4cc2b67c27cf17f000f94beb43cf2757924cc
Lindsayy121 <116232724+Lindsayy121@users.noreply.github.com>
2023-05-17 09:58:23 -0700

    Started GameManager and PlayerControl Script
    Needs more work

---
+++ ChocolateRoadPeriod2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+    public TextMeshProUGUI gameOverText;
+    private int lives;
+    public bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverText.gameObject.SetActive(true);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        score = 0;
+        UpdateScore(0);
+        UpdateLives(3);
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            UpdateScore(5);
+        }
+    }
+
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+    }
+
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ChocolateRoadPeriod2/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Animator playerAnim;
+    private Rigidbody playerRb;
+    public bool gameOver = false;
+    public bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstcale"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+    }
+}
