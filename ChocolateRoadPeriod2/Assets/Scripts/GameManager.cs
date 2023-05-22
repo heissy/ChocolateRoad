5baf9d16b582324bdf51a764bc5ed1a58d521c4b
Lindsayy121 <116232724+Lindsayy121@users.noreply.github.com>
2023-05-22 12:17:11 -0700

    Working on movement and title
    needs works

--- ChocolateRoadPeriod2/Assets/Scripts/GameManager.cs
+++ ChocolateRoadPeriod2/Assets/Scripts/GameManager.cs
@@ -2,42 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
     private int score;
     public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI livesText;
     public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public TextMeshProUGUI livesText;
     private int lives;
-    public bool gameOver;
+    public GameObject pauseScreen;
+    private bool paused;
 
     // Start is called before the first frame update
     void Start()
     {
-        UpdateScore(0);
-    }
-
-    public void StartGame(int difficulty)
-    {
-
-        score = 0;
-        UpdateScore(0);
-        UpdateLives(3);
         
-    }
-
-    IEnumerator SpawnTarget()
-    {
-        while(true)
-        {
-            UpdateScore(5);
-        }
-    }
-
-    private void UpdateScore(int scoreToAdd)
-    {
-        score += scoreToAdd;
     }
 
     public void UpdateLives(int livesToChange)
@@ -46,14 +31,70 @@ public class GameManager : MonoBehaviour
         livesText.text = "Lives: " + lives;
         if (lives <= 0)
         {
-            
+            GameOver();
         }
     }
 
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        spawnRate /= difficulty;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+        UpdateLives(3);
+    }
 
-    // Update is called once per frame
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score:" + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- ChocolateRoadPeriod2/Assets/Scripts/PlayerControl.cs
+++ ChocolateRoadPeriod2/Assets/Scripts/PlayerControl.cs
@@ -4,65 +4,25 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    private Rigidbody playerRb;
-    public float jumpForce;
-    public float gravityModifier;
-    public bool isOnGround = true;
-    private float speed = 30;
-    public bool gameOver = false;
-    private Animator playerAnim;
-    public ParticleSystem explosionParticle;
-    public ParticleSystem dirtParticle;
-    public AudioClip crashSound;
-    private AudioSource playerAudio;
-    public float forwardInput;
-    public float horizontalInput;
-    public float turnSpeed;
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
-        playerAnim = GetComponent<Animator>();
-        playerAudio = GetComponent<AudioSource>();
-        Physics.gravity *= gravityModifier;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
-        {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
-            dirtParticle.Stop();
+        //Move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-            horizontalInput = Input.GetAxis("Horizontal");
-            forwardInput = Input.GetAxis("Vertical");
-
-
-            transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-            transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-            dirtParticle.Play();
-        }
-
-        else if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            gameOver = true;
-            Debug.Log("Game Over!");
-            playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1);
-            explosionParticle.Play();
-            dirtParticle.Stop();
-            playerAudio.PlayOneShot(crashSound, 1.0f);
-        }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
